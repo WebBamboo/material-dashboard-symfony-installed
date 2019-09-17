8bd884f039fcc64cb51cf619b4f3e8a474f56e75
Pavel Petrov <pavel.petrov.wb@gmail.com>
2019-09-17 14:29:30 +0300

    Landing pages, DX changes, general improvements

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,6 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('admin/home.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
      * @Route("/admin", name="admin")
      */
     public function index()
